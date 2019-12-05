83c3795f975fd6b429a68b80141703d65ac8d578
wood.zeng <635729656@qq.com>
2019-12-05 18:29:45 +0800

    集成 Swoole 实现 WebSocket 服务器

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,47 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="UTF-8">
+   <title>Chat Client</title>
+</head>
+<body>
+<script>
+   window.onload = function () {
+       var nick = prompt("Enter your nickname");
+       var input = document.getElementById("input");
+       input.focus();
 
-        <title>Laravel</title>
+       // 初始化客户端套接字并建立连接
+       var socket = new WebSocket("ws://swoole-xueyuanjun.laravel.host/ws");
+       
+       // 连接建立时触发
+       socket.onopen = function (event) {
+           console.log("Connection open ..."); 
+       }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+       // 接收到服务端推送时执行
+       socket.onmessage = function (event) {
+           var msg = event.data;
+           var node = document.createTextNode(msg);
+           var div = document.createElement("div");
+           div.appendChild(node);
+           document.body.insertBefore(div, input);
+           input.scrollIntoView();
+       };
+       
+       // 连接关闭时触发
+       socket.onclose = function (event) {
+           console.log("Connection closed ..."); 
+       }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+       input.onchange = function () {
+           var msg = nick + ": " + input.value;
+           // 将输入框变更信息通过 send 方法发送到服务器
+           socket.send(msg);
+           input.value = "";
+       };
+   }
+</script>
+<input id="input" style="width: 100%;">
+</body>
 </html>
